f16720fa44bd297e938e4162d87f6e5dae104c51
Rusty Bentley <mycoding.alert@gmail.com>
2022-07-18 17:36:05 -0700

    adds flapping

---
+++ fifty-bird/bird04/main.lua
@@ -0,0 +1,70 @@
+push = require 'push'
+Class = require 'class'
+require 'Bird'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+BACKGROUND_SCROLL_SPEED = 30
+GROUND_SCROLL_SPEED = 60
+
+BACKGROUND_LOOPING_POINT = 413
+
+local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+local bird = Bird()
+
+function love.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+
+  love.window.setTitle('Fifty Bird')
+
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    vsync = true,
+    fullscreen = false,
+    resizable = true
+  })
+
+  love.keyboard.keysPressed = {}
+end
+
+function love.update(dt)
+  backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+      % BACKGROUND_LOOPING_POINT
+
+  groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+      % VIRTUAL_WIDTH
+
+  bird:update(dt)
+  love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+  push:resize(w, h)
+end
+
+function love.keypressed(key)
+  love.keyboard.keysPressed[key] = true
+
+  if key == 'escape' then
+    love.event.quit()
+  end
+end
+
+function love.keyboard.wasPressed(key)
+  return love.keyboard.keysPressed[key]
+end
+
+function love.draw()
+  push:start()
+  love.graphics.draw(background, -backgroundScroll, 0)
+  love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+  bird:render()
+  push:finish()
+end
