a17a51b8c972b65af035182664c9b026ccf6ca68
Rusty Bentley <mycoding.alert@gmail.com>
2022-07-19 20:36:24 -0700

    removes unnecessary hiding code

--- fifty-bird/bird-assignment/states/ScoreState.lua
+++ fifty-bird/bird-assignment/states/ScoreState.lua
@@ -11,27 +11,10 @@
 ScoreState = Class { __includes = BaseState }
 
 local medals = {
-  ['gold'] = {
-    ['image'] = love.graphics.newImage('gold.png'),
-    ['drawn'] = false
-  },
-  ['silver'] = {
-    ['image'] = love.graphics.newImage('silver.png'),
-    ['drawn'] = false
-  },
-  ['bronze'] = {
-    ['image'] = love.graphics.newImage('bronze.png'),
-    ['drawn'] = false
-  }
+  ['gold'] = love.graphics.newImage('gold.png'),
+  ['silver'] = love.graphics.newImage('silver.png'),
+  ['bronze'] = love.graphics.newImage('bronze.png')
 }
-
-function ScoreState:hideMedals()
-  for _, medal in pairs(medals) do
-    if medal['drawn'] then
-      medal['image'].visible = false
-    end
-  end
-end
 
 --[[
     When we enter the score state, we expect to receive the score
@@ -44,17 +27,12 @@ end
 function ScoreState:update(dt)
   -- go back to play if enter is pressed
   if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
-
     gStateMachine:change('countdown')
   end
 end
 
-local function showOrDrawMedal(medal)
-  if medal['drawn'] then
-    medal['image'].visible = true
-  else
-    love.graphics.draw(medal['image'], VIRTUAL_WIDTH / 2 - 20, 120)
-  end
+local function drawMedal(medal)
+  love.graphics.draw(medal, VIRTUAL_WIDTH / 2 - 20, 120)
 end
 
 function ScoreState:render()
@@ -66,11 +44,11 @@ function ScoreState:render()
   love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
 
   if self.score >= 8 then
-    showOrDrawMedal(medals['gold'])
+    drawMedal(medals['gold'])
   elseif self.score >= 5 then
-    showOrDrawMedal(medals['silver'])
+    drawMedal(medals['silver'])
   elseif self.score >= 3 then
-    showOrDrawMedal(medals['bronze'])
+    drawMedal(medals['bronze'])
   end
 
   love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
